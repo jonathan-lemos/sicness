882a5706b750a3853eb02797c894c5adebcd9e96
Jonathan Lemos <jonathanlemos@protonmail.com>
2018-10-10 10:57:55 -0400

    adding compiler

--- js/sicxe_cpu.js
+++ js/sicxe_cpu.js
@@ -229,7 +229,7 @@ let sicxe_cpu.prototype.opcodes["ADDR"] = (reg1, reg2) => {
 	this.__sic_correct_flow(reg2);
 }
 
-let sicxe_cpu.prototype.opcodes["AND"] = (mem_loc) => {
+let sic_cpu.prototype.opcodes["AND"] = (mem_loc) => {
 	this.__sic_validate_addr(mem_loc);
 
 	this.registers["A"] &= this.__sic_deref24(mem_loc);
@@ -298,7 +298,7 @@ let sic_cpu.prototype.opcodes["JEQ"] = (mem_loc) => {
 	this.__sic_validate_addr(mem_loc);
 
 	if (this.registers["SW"] === '='){
-		sic_J(mem_loc);
+		this.opcodes["J"](mem_loc);
 	}
 }
 
@@ -306,7 +306,7 @@ let sic_cpu.prototype.opcodes["JGT"] = (mem_loc) => {
 	this.__sic_validate_addr(mem_loc);
 
 	if (this.registers["SW"] === '>'){
-		sic_J(mem_loc);
+		this.opcodes["J"](mem_loc);
 	}
 }
 
@@ -314,7 +314,7 @@ let sic_cpu.prototype.opcodes["JLE"] = (mem_loc) => {
 	this.__sic_validate_addr(mem_loc);
 
 	if (this.registers["SW"] === '<'){
-		sic_J(mem_loc);
+		this.opcodes["J"](mem_loc);
 	}
 }
 
@@ -398,7 +398,7 @@ let sic_cpu.prototype.opcodes["RD"] = (dev_name) => {
 		throw "There is no data left in " + dev_name;
 	}
 
-	sic_LDCH(ch);
+	this.opcodes["LDCH"](ch);
 }
 
 let sicxe_cpu.prototype.opcodes["RMO"] = (reg1, reg2) => {
@@ -436,69 +436,233 @@ let sicxe_cpu.prototype.opcodes["SHIFTR"] = (reg, n) => {
 	this.registers[reg] >>= n;
 }
 
-let sic_cpu.prototype.opcodes["STA(mem_loc){
+let sic_cpu.prototype.opcodes["STA"] = (mem_loc) => {
+	this.__sic_validate_addr(mem_loc);
+
 	this.__sic_place24(this.registers["A"], mem_loc);
 }
 
-function sicxe_STB(mem_loc){
+let sicxe_cpu.prototype.opcodes["STB"] = (mem_loc) => {
+	this.__sic_validate_addr(mem_loc);
+
 	this.__sic_place24(this.registers["B"], mem_loc);
 }
 
-function sic_STCH(mem_loc){
+let sic_cpu.prototype.opcodes["STCH"] = (mem_loc) => {
+	this.__sic_validate_addr(mem_loc);
+
 	this.__sic_place8(this.registers["A"] & 0xFF, mem_loc);
 }
 
-function sic_STL(mem_loc){
+let sicxe_cpu.prototype.opcodes["STI"] = (mem_loc) => {
+	throw "sti not implemented yet";
+}
+
+let sic_cpu.prototype.opcodes["STL"] = (mem_loc) => {
+	this.__sic_validate_addr(mem_loc);
+
 	this.__sic_place24(this.registers["L"], mem_loc);
 }
 
-function sic_STSW(mem_loc){
+let sic_cpu.prototype.opcodes["STSW"] = (mem_loc) => {
 	throw "stsw not implemented yet";
 }
 
-function sic_STX(mem_loc){
-	this.__sic_place24(this.registers["X"]);
+let sicxe_cpu.prototype.opcodes["STT"] = (mem_loc) => {
+	this.__sic_validate_addr(mem_loc);
+
+	this.__sic_place24(this.registers["T"], mem_loc);
 }
 
-function sic_SUB(mem_loc){
+let sic_cpu.prototype.opcodes["STX"] = (mem_loc) => {
+	this.__sic_validate_addr(mem_loc);
+
+	this.__sic_place24(this.registers["X"], mem_loc);
+}
+
+let sic_cpu.prototype.opcodes["SUB"] = (mem_loc) => {
+	this.__sic_validate_addr(mem_loc);
+
 	this.registers["A"] -= this.__sic_deref24(mem_loc);
 	this.__sic_correct_flow("A");
 }
 
-function sic_TD(dev_no){
-	throw "TD not implemented";
+let sicxe_cpu.prototype.opcodes["SUBR"] = (reg1, reg2) => {
+	this.__sic_validate_reg(reg1);
+	this.__sic_validate_reg(reg2);
+
+	this.registers[reg2] -= this.registers[reg1];
+	this.__sic_correct_flow(reg2);
 }
 
-function sic_TIX(mem_loc){
+let sicxe_cpu.prototype.opcodes["SVC"] = (n) => {
+	throw "svc not implemented yet";
+}
+
+let sic_cpu.prototype.opcodes["TD"] = (dev_name) => {
+	if (typeof dev_name !== "string"){
+		throw "dev_name must be a string";
+	}
+	if (this.devices[dev_name] === undefined){
+		throw "dev " + dev_name + " does not exist";
+	}
+
+	if (this.devices[dev_name].writing){
+		return true;
+	}
+
+	if (this.devices[dev_name].contents[0] === undefined){
+		return false;
+	}
+	return true;
+}
+
+let sic_cpu.prototype.opcodes["TIX"] = (mem_loc) => {
+	this.__sic_validate_addr(mem_loc);
+
 	this.registers["X"]++;
-	sic_COMP(mem_loc);
+	this.opcodes["COMP"](mem_loc);
 }
 
-function sic_WD(dev_no){
-	if (dev_no < 4 || dev_no > 6){
-		throw "Bad device number"
-	}
-	devices[dev_no].push(this.registers["A"] & 0xFF);
+let sicxe_cpu.prototype.opcodes["TIXR"] = (reg) => {
+	this.__sic_validate_reg(reg);
+
+	this.registers["X"]++;
+	this.opcodes["COMPR"](reg);
 }
 
+let sic_cpu.prototype.opcodes["WD"] = (dev_name) => {
+	this.__sic_validate_wrdev(dev_name);
 
-function sic_cycle(){
-
+	devices[dev_name].push(this.registers["A"] & 0xFF);
 }
 
-function sic_init(c_dev00, c_devf1, c_devf2, c_devf3){
-	sic_cpu_reset();
-	sic_file_reset(c_dev00, c_devf1, c_devf2, c_devf3);
-	// if dev00 is longer than 128 bytes
-	if (dev00.length > 0x7F){
-		throw "dev00 is too long";
+let __sic_hex_to_opcode = (m_byte) => {
+	if (typeof m_byte !== "number"){
+		throw "m_byte is not a number";
+	}
+	if (m_byte < 0x00 || m_byte > 0xFF){
+		throw "m_byte has to be in range [0x00-0xFF]"
 	}
 
-	if (dev00 == null){
-		throw "dev00 does not exist";
-	}
-
-	for (var i = 0; i < dev00.length; ++i){
-		memory[i] = dev00[i];
+	switch (m_byte){
+	case 0x18:
+		return ["ADD", [3, 4]];
+	case 0x58:
+		return ["ADDF", [3, 4]];
+	case 0x90:
+		return ["ADDR", [2]];
+	case 0x40:
+		return ["AND", [3, 4]];
+	case 0xB4:
+		return ["CLEAR", [2]];
+	case 0x28:
+		return ["COMP", [3, 4]];
+	case 0x88:
+		return ["COMPF", [3, 4]];
+	case 0xA0:
+		return ["COMPR", [2]];
+	case 0x24:
+		return ["DIV", [3, 4]];
+	case 0x64:
+		return ["DIVF", [3, 4]];
+	case 0x9C:
+		return ["DIVR", [2]];
+	case 0xC4:
+		return ["FIX", [1]];
+	case 0xC0:
+		return ["FLOAT", [1]];
+	case 0xF4:
+		return ["HIO", [1]];
+	case 0x3C:
+		return ["J", [3, 4]];
+	case 0x30:
+		return ["JEQ", [3, 4]];
+	case 0x38:
+		return ["JLT", [3, 4]];
+	case 0x48:
+		return ["JSUB", [3, 4]];
+	case 0x00:
+		return ["LDA", [3, 4]];
+	case 0x68:
+		return ["LDB", [3, 4]];
+	case 0x50:
+		return ["LDCH", [3, 4]];
+	case 0x70:
+		return ["LDF", [3, 4]];
+	case 0x08:
+		return ["LDL", [3, 4]];
+	case 0x6C:
+		return ["LDS", [3, 4]];
+	case 0x74:
+		return ["LDT", [3, 4]];
+	case 0x04:
+		return ["LDX", [3, 4]];
+	case 0xD0:
+		return ["LPS", [3, 4]];
+	case 0x20:
+		return ["MUL", [3, 4]];
+	case 0x60:
+		return ["MULF", [3, 4]];
+	case 0x98:
+		return ["MULR", [2]];
+	case 0xC8:
+		return ["NORM", [1]];
+	case 0x44:
+		return ["OR", [3, 4]];
+	case 0xD8:
+		return ["RD", [3, 4]];
+	case 0xAC:
+		return ["RMO", [2]];
+	case 0x4C:
+		return ["RSUB", [3, 4]];
+	case 0xA4:
+		return ["SHIFTL", [2]];
+	case 0xA8:
+		return ["SHIFTR", [2]];
+	case 0xF0:
+		return ["SIO", [1]];
+	case 0xEC:
+		return ["SSK", [3, 4]];
+	case 0x0C:
+		return ["STA", [3, 4]];
+	case 0x78:
+		return ["STB", [3, 4]];
+	case 0x54:
+		return ["STCH", [3, 4]];
+	case 0x80:
+		return ["STF", [3, 4]];
+	case 0xD4:
+		return ["STI", [3, 4]];
+	case 0x14:
+		return ["STL", [3, 4]];
+	case 0x7C:
+		return ["STS", [3, 4]];
+	case 0xE8:
+		return ["STSW", [3, 4]];
+	case 0x84:
+		return ["STT", [3, 4]];
+	case 0x10:
+		return ["STX", [3, 4]];
+	case 0x1C:
+		return ["SUB", [3, 4]];
+	case 0x5C:
+		return ["SUBF", [3, 4]];
+	case 0x94:
+		return ["SUBR", [2]];
+	case 0xB0:
+		return ["SVC", [2]];
+	case 0xE0:
+		return ["TD", [3, 4]];
+	case 0xF8:
+		return ["TIO", [1]];
+	case 0x2C:
+		return ["TIX", [3, 4]];
+	case 0xB8:
+		return ["TIXR", [2]];
+	case 0xDC:
+		return ["WD", [3, 4]];
+	default:
+		throw "unknown opcode";
 	}
 }
